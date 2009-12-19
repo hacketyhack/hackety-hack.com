99cfc2a1e0e68405f68600837db084d30b4dc499
Steve Klabnik <steve@steveklabnik.com>
2009-12-18 23:29:21 -0500

    basic user stuff

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,4 +7,15 @@ class ApplicationController < ActionController::Base
 
   # Scrub sensitive parameters from your log
   # filter_parameter_logging :password
+	helper_method :current_user
+
+	private
+	def current_user_session
+		return @current_user_session if defined?(@current_user_session)  
+		@current_user_session = UserSession.find  
+	end  
+
+	def current_user  
+		@current_user = current_user_session && current_user_session.record  
+	end  
 end
---
+++ db/migrate/20091219034919_create_users.rb
@@ -0,0 +1,18 @@
+class CreateUsers < ActiveRecord::Migration
+  def self.up
+    create_table :users do |t|
+      t.string :username
+      t.string :email
+      t.text :about
+      t.string :link
+      t.string :crypted_password
+      t.string :password_salt
+      t.string :persistence_token
+      t.timestamps
+    end
+  end
+
+  def self.down
+    drop_table :users
+  end
+end
