40d247e814ffc40dc55ce93b12d98a989f09bc3d
Steve Klabnik <steve@steveklabnik.com>
2010-06-16 19:42:47 -0400

    posts now in reverse chronological order.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,7 +2,7 @@ class PostsController < ApplicationController
   # GET /posts
   # GET /posts.xml
   def index
-    @posts = Post.all
+    @posts = Post.all.reverse
 
     respond_to do |format|
       format.html # index.html.erb
