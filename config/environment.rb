f654ea009608f2faf12f5234b1a31d9b3c74b0b7
Steve Klabnik <steve@steveklabnik.com>
2010-06-26 00:38:23 -0400

    moving to haml, and adding a screenshot to the main page

--- config/environment.rb
+++ config/environment.rb
@@ -1,7 +1,7 @@
 # Be sure to restart your server when you modify this file
 
 # Specifies gem version of Rails to use when vendor/rails is not present
-RAILS_GEM_VERSION = '2.3.5' unless defined? RAILS_GEM_VERSION
+RAILS_GEM_VERSION = '2.3.8' unless defined? RAILS_GEM_VERSION
 
 # Bootstrap the Rails environment, frameworks, and default configuration
 require File.join(File.dirname(__FILE__), 'boot')
@@ -14,6 +14,7 @@ Rails::Initializer.run do |config|
 	config.gem "syntax"
   config.gem "bluecloth"
   config.gem "rdiscount"
+  config.gem "haml"
 
 end
 
