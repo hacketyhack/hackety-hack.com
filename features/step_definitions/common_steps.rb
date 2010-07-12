fd4b66a8cf76b7aadf5f49cadf9cb4d32d4db472
Steve Klabnik <steve@steveklabnik.com>
2010-07-11 21:17:29 -0400

    New authentication system.
    I got fed up with the lack of flexibility of sinatra-authentication, so
    I just rolled my own auth system. Probably a good idea to show everyone
    how it works anyway...

--- features/step_definitions/common_steps.rb
+++ features/step_definitions/common_steps.rb
@@ -1,6 +1,6 @@
 def login user
 	visit "/login"
-	fill_in "email", :with => user.email
+	fill_in "username", :with => user.username
 	fill_in "password", :with => user.password
 	click_button "login"
 end
--- models/mm_user.rb
+++
@@ -1,3 +0,0 @@
-class MmUser
-	key :username, String, :unique => true
-end
