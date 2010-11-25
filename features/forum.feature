cea51ec8091c65e36fa119d04dedd970d6ca627d
Steve Klabnik <steve@steveklabnik.com>
2010-11-25 18:05:03 -0500

    making new discussions

--- features/forum.feature
+++ features/forum.feature
@@ -1,16 +1,4 @@
 Feature: The Hackety Forum
-  Scenario: Make a new discussion
-    Given I'm logged in as "some_hacker"
-    And I go to the forums
-    And I follow "Learning Ruby"
-    When I follow "New Discussion"
-    And I fill in "Title" with "HALP!"
-    And I fill in "Body" with "I'm really unsure as to how to do something."
-    And I press "Submit"
-    Then I should be on the discussion "halp" in "learning_ruby"
-    And I should see "Discussion created!"
-    And I should see "HALP!"
-    And I should see "By some_hacker"
   Scenario: Can't make discussions when logged out
     Given I'm not logged in
     And I go to the forums
--- spec/acceptance/forum_spec.rb
+++ spec/acceptance/forum_spec.rb
@@ -48,7 +48,24 @@ feature "Forums" do
     should_be_on "/forums/learning_ruby/i_need_help"
     page.should have_content "Replied!"
     page.should have_content "I'll help you"
-    page.should have_content "steve says:"
+    page.should have_content "#{@hacker.username} says:"
+  end
+
+  scenario "make a new discussion" do
+    @hacker = Factory(:hacker)
+    log_in @hacker
+
+    visit "/forums/learning_ruby"
+    click_link "New Discussion"
+
+    fill_in "Title", :with => "HALP!"
+    fill_in "Body", :with => "I'm really unsure as to how to do something."
+    click_button "Submit"
+
+    should_be_on "/forums/learning_ruby/halp"
+    page.should have_content "Discussion created!"
+    page.should have_content "HALP!"
+    page.should have_content "By #{@hacker.username}"
   end
 
 end
