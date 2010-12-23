0ec515bd8d71297c4cd4dd25e5d3ac4bd81dadd3
Steve Klabnik <steve@steveklabnik.com>
2010-12-22 23:21:52 -0500

    login and signup are working

--- Gemfile
+++ Gemfile
@@ -1,5 +1,32 @@
+#The first part of any Gemfile is to list sources.
+#Most gems are located at rubygems.org
 source "http://rubygems.org"
 
-gem "sinatra"
-gem "haml"
+#after that, we use the 'gem' statement to list what gems we want to use.
+#we can also add version requirements so that we get the exact version.
+
+#see more at http://gembundler.com/
+
+gem "sinatra", "~>1.1"
+gem "haml", "~>3.0.18"
+gem "mongo_mapper", "~>0.8.4"
+gem "rack-flash", "~>0.1.1"
+gem "rdiscount", "~>1.6.5"
+gem "i18n", "~>0.4.1"
+gem "bson_ext", "~>1.1"
+gem "pony", "~>1.0.1"
 gem "sinatra-content-for"
+
+group :test do
+  gem "launchy", "~>0.3.7"
+  gem "autotest", "~>4.3.2"
+  gem "capybara", "~>0.3.9"
+  gem "factory_girl", "~>1.3.2"
+  gem "database_cleaner", "~>0.6.0"
+  gem "rspec", "~>2.1.0"
+  gem "webrat", "~>0.7.1"
+  gem "steak", "~>1.0.0"
+
+  gem "ruby-debug"
+end
+
--- app.rb
+++ app.rb
@@ -1,12 +1,80 @@
 #!/usr/bin/env ruby
 # encoding: utf-8
 
+#first off, we need to include rubygems and sinatra
+require 'rubygems'
+
+#this bundler stuff needed for heroku
+require 'bundler'
+Bundler.setup
+
 require 'sinatra'
+
+#this lets us send emails
+require 'pony'
+
+#haml gives us all of our templates
 require 'haml'
-require 'sass'
+
+#mongomapper connects us to our database
+require 'mongo_mapper'
+
+#rack-flash gives us nice flash messages
+require 'rack-flash'
+
+#rdiscount lets us write things using markdown
+require 'rdiscount'
+
+#we need to set up a secret to encrypt our sessions with
+use Rack::Session::Cookie, :secret => 'h4ck3ty h4ck f0r gr347 g00d'
+
+#we also have to let the world know we want to use flashes
+use Rack::Flash
+
 require 'sinatra/content_for'
 
-set :port, 9000
+require 'helpers'
+
+#this makes Haml escape any html by default. See here: http://haml-lang.com/docs/yardoc/file.HAML_REFERENCE.html#options
+set :haml, :escape_html => true
+
+#these configure blocks only run in one environment
+configure :test do
+  PONY_VIA_OPTIONS = {}
+end
+
+configure :development do
+  PONY_VIA_OPTIONS = {}
+end
+
+configure :production do
+
+  PONY_VIA_OPTIONS =  {
+    :address        => "smtp.sendgrid.net",
+    :port           => "25",
+    :authentication => :plain,
+    :user_name      => ENV['SENDGRID_USERNAME'],
+    :password       => ENV['SENDGRID_PASSWORD'],
+    :domain         => ENV['SENDGRID_DOMAIN']
+  }
+  
+end
+
+#for all environments,
+configure do
+  if ENV['MONGOHQ_URL']
+    MongoMapper.config = {ENV['RACK_ENV'] => {'uri' => ENV['MONGOHQ_URL']}}
+    MongoMapper.database = ENV['MONGOHQ_DATABASE']
+    MongoMapper.connect("production")
+  else
+    MongoMapper.connection = Mongo::Connection.new('localhost')
+    MongoMapper.database = "hackety-development"
+  end
+  #require all of our models!
+  require_directory "models"
+end
+
+require_directory "controllers"
 
 get '/' do
   haml :index
@@ -20,6 +88,6 @@ get '/download' do
   haml :download
 end
 
-get '/feed' do
-  haml :feed
+get '/stream' do
+  haml :stream
 end
