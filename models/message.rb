938fdf8389236bcb7b3aeee8d602b4d29116ce23
Steve Klabnik <steve@steveklabnik.com>
2010-12-23 15:21:57 -0500

    Messages are back

---
+++ models/message.rb
@@ -0,0 +1,27 @@
+#this is the class for inter-site messages
+class Message
+  #we want to include them in the database!
+  include MongoMapper::Document
+
+  #this is the body text of the message
+  key :body, String
+
+  #this is the username of the person who gets the message
+  key :recipient, String
+
+  #this is the username of the person who sent the message
+  key :sender, String
+
+  after_create :send_notification
+
+  private
+
+  def send_notification
+    unless development?
+      recipient_email = Hacker.first(:username => self.recipient).email
+      Notifier.send_message_notification(recipient_email, self.sender)
+    end
+  end
+
+end
+
---
+++ models/notifier.rb
@@ -0,0 +1,17 @@
+class Notifier 
+  def self.send_message_notification(recipient, who)
+    Pony.mail(:to => recipient, 
+              :subject => "Hackety Hack: New Message",
+              :from => "steve+hackety@steveklabnik.com",
+              :body => render_haml_template("message", who),
+              :via => :smtp, :via_options => PONY_VIA_OPTIONS)
+  end
+
+  private
+
+  def self.render_haml_template(template, who)
+    engine = Haml::Engine.new(File.open("views/notifier/#{template}.haml", "rb").read)
+    engine.render(Object.new, :who => who)
+  end
+end
+
