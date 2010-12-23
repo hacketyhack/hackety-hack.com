8978821668d3dbb7cd758eccf626c978ccb7ce1d
Steve Klabnik <steve@steveklabnik.com>
2010-12-23 14:51:02 -0500

    progams stuff added

---
+++ controllers/programs.rb
@@ -0,0 +1,54 @@
+get "/programs/new" do
+  require_login!
+  haml :"programs/new"
+end
+
+post "/programs" do
+  require_login_or_api! :username => params[:username], :password => params[:password]
+  params[:program]['creator_username'] = current_user.username
+  program = Program.create(params[:program])
+  flash[:notice] = "Program created!"
+  redirect "/programs/#{program.creator_username}/#{program.slug}"
+end
+
+get "/programs/:username.json" do
+  programs = Program.all(:creator_username => params[:username])
+  programs.to_json
+end
+
+get "/programs/:username/:slug" do
+  @program = Program.first(:creator_username => params[:username], :slug => params[:slug])
+  haml :"programs/show"
+end
+
+put "/programs/:username/:slug.json" do
+  require_login_or_api! :username => params[:username], :password => params[:password]
+  if current_user.username != params[:username]
+    redirect "/"
+  end
+  program = Program.first(:creator_username => params[:username], :slug => params[:slug])
+  if program.nil?
+    program = Program.create(params)
+  else
+    program.update_attributes(params)
+    program.save
+  end
+
+  flash[:notice] = "Program updated!"
+  redirect "/programs/#{program.creator_username}/#{program.slug}"
+end
+
+put "/programs/:username/:slug" do
+  require_login_or_api! :username => params[:username], :password => params[:password]
+  if current_user.username != params[:username]
+    flash[:notice] = "Sorry, buddy"
+    redirect "/"
+  end
+  program = Program.first(:creator_username => params[:username], :slug => params[:slug])
+  program.update_attributes(params[:program])
+  program.save
+
+  flash[:notice] = "Program updated!"
+  redirect "/programs/#{program.creator_username}/#{program.slug}"
+end
+
