1ba225303a4e346290ab6dccc1b05f02bcc36d97
Steve Klabnik <steve@steveklabnik.com>
2011-01-06 14:50:44 -0500

    Writing documentation for our helpers file.

--- helpers.rb
+++ helpers.rb
@@ -1,56 +1,57 @@
-#this helpers file contains lots of helpful little methods
-#to make our work with sinatra easier
+# The **helpers.rb** file contains all of our Sinatra helpers. This is large
+# enough that including it in a separate file makes it much easier to find;
+# otherwise, I'd be opening **hackety.rb** and searching for 'helpers,' and
+# that's just stupid.
+
 
 helpers do
 
-  #this defines three helpers, that all test our environment:
-  #they're called 'development?', 'test?', and 'production?'
+  # A tiny bit of metaprogramming goes a long way. We want to generate three
+  # methods (`development?`, `production?`, and `test?`) that let us know which
+  # environment we happen to be in. This is useful in a few places.
   [:development, :production, :test].each do |environment|
     define_method "#{environment.to_s}?" do
       return settings.environment == environment.to_sym
     end
   end
 
-  #this method returns the logged_in hacker
+  # This incredibly useful helper gives us the currently logged in user. We
+  # keep track of that by just setting a session variable with their id. If it
+  # doesn't exist, we just want to return nil.
   def current_user
-    #let's look up the Hacker by the id in the session
     return Hacker.first(:id => session[:hacker_id]) if session[:hacker_id]
-
-    #if we can't find them, just return nil
     nil
   end
 
-  #this method returns true if we're logged in, and false if we're not
+  # This very simple method checks if we've got a logged in user. That's pretty
+  # easy: just check our current_user.
   def logged_in?
-    #pretty easy, just check the session
     current_user != nil
   end
 
-  #this helper checks if the current_user is admin, and redirects them if they're not
+  # Our `admin_only!` helper will only let admin users visit the page. If
+  # they're not an admin, we redirect them to either / or the page that we
+  # specified when we called it.
   def admin_only!(opts = {:return => "/"})
-    #we need to be both logged_in and an admin for this to work
     unless logged_in? && current_user.admin?
-      #if we're not, set an error message
       flash[:error] = "Sorry, buddy"
-
-      #and get redirected
       redirect opts[:return]
     end
   end
 
-  #this method makes sure that we're logged in
+  # Similar to `admin_only!`, `require_login!` only lets logged in users access
+  # a particular page, and redirects them if they're not.
   def require_login!(opts = {:return => "/"})
-    #if we're not
     unless logged_in?
-      #set an error message
       flash[:error] = "Sorry, buddy"
-
-      #and get redirected
       redirect opts[:return]
     end
   end
 
-  #this method lets us use an api call as well as logging in
+  # We also want to have a way for the desktop application to make calls to the
+  # site. For this, we allow a username and password to be passed in, and we
+  # authenticate directly, rather than relying on a previously logged in
+  # session.
   def require_login_or_api!(opts = {:return => "/"})
     hacker = Hacker.authenticate(opts[:username], opts[:password])
     if hacker
@@ -60,7 +61,9 @@ helpers do
     end
   end
 
-  #gives a gravatar url for an email
+  # [Gravatar](http://gravatar.com/) is used for our avatars. Generating the
+  # url for one is pretty simple, we just need the proper email address, and
+  # then we make an md5 of it. No biggie.
   def gravatar_url_for email
     require 'digest/md5'
     "http://www.gravatar.com/avatar/#{Digest::MD5.hexdigest(email.downcase)}"
@@ -68,17 +71,16 @@ helpers do
 
 end
 
-#this lets us require a whole directory of .rb files!
+# This handy helper method lets us require an entire directory of `rb` files.
+# It's much simpler than having to require them all directly.
 def require_directory dirname
-
-  #we glob every file, and loop through them
   Dir.glob("#{File.expand_path(File.dirname(__FILE__))}/#{dirname}/*.rb").each do |f|
-
-    #and then require them!
     require f
   end
 end
 
+# This method is a handy monkeypatch on String. It allows us to turn any string
+# into a slug that's suitable for putting into URLs.
 class String
   def to_slug
     self.gsub(/[^a-zA-Z _0-9]/, "").gsub(/\s/, "_").downcase
