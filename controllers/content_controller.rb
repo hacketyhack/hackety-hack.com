df8944dacbb30aa79e0885242e47e49333628e3f
Steve Klabnik <steve@steveklabnik.com>
2011-01-17 10:38:15 -0500

    requiring login to post content

--- controllers/content_controller.rb
+++ controllers/content_controller.rb
@@ -9,17 +9,10 @@ end
 
 # We also need a simple POST that will create content.
 post "/content" do
+  require_login!
 
-  # One small wrinkle: We're allowing anonymous posts. So, if we're not logged
-  # in, we need to set the author and email properly so that all of our views
-  # work.
-  if current_user
-    params[:content][:author] = current_user.username
-    params[:content][:author_email] = current_user.email
-  else
-    params[:content][:author] = "anonymous"
-    params[:content][:author_email] = "anonymous@example.com"
-  end
+  params[:content][:author] = current_user.username
+  params[:content][:author_email] = current_user.email
   @content = Content.create(params[:content])
   flash[:notice] = "Thanks for your post!"
   redirect "/stream"
@@ -28,17 +21,11 @@ end
 # We're allowing comments to be made on posts, so we need a route for that as
 # well.
 post "/content/:id/comment" do
+  require_login!
   @content = Content.first(:id => params[:id])
 
-  # We have the same wrinkle as we do when making posts: We're allowing
-  # anonymous comments. So we have to properly set all that up.
-  if current_user
-    params[:comment][:author] = current_user.username
-    params[:comment][:author_email] = current_user.email
-  else 
-    params[:comment][:author] = "Anonymous"
-    params[:comment][:author_email] = "anonymous@example.com"
-  end
+  params[:comment][:author] = current_user.username
+  params[:comment][:author_email] = current_user.email
   @content.comments << Comment.new(params[:comment])
   @content.save
 
