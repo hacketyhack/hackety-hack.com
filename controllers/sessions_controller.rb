9569f597cd59eebdf80dff32ebd60ed69285c841
Steve Klabnik <steve@steveklabnik.com>
2011-01-17 21:10:07 -0500

    Adding signup via the API.

--- controllers/sessions_controller.rb
+++ controllers/sessions_controller.rb
@@ -66,10 +66,28 @@ end
 # by the desktop app to see if you've entered correct information.
 post '/check_credentials' do
   #let's see if we got a correct username/password:
-  puts "username: #{params[:username]} / #{params[:password]}"
   if Hacker.authenticate(params[:username], params[:password])
     "Success"
   else
     halt 401
+  end
+end
+
+# This method will let people sign up from within the app. Frankly this is
+# a really bad way to do it, but I'm not sure what the best way is. It should
+# be combined with the signup method above. The issue is that signup via the
+# API should really just throw the message rather than redirect, but the site
+# should redirect. This probably should be managed via content negotiation. I'm
+# doing it the dirty way now, and when I look at exposing a Real Api I'll worry
+# about making it 100% nice.
+post '/signup_via_api' do
+  @hacker = Hacker.create(:username => params[:username],
+                          :email => params[:email],
+                          :password => params[:password])
+
+  if @hacker && @hacker.valid?
+    "Success"
+  else
+    404 #is this right?
   end
 end
