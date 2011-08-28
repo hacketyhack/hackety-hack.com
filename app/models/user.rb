421ae23bca1c50527dbd887799c3c2a3a069f4bb
Dominic Dagradi <dominic@bearded.com>
2011-08-28 15:48:45 -0400

    added devise w/ mongo_mapper support for User

---
+++ app/models/user.rb
@@ -0,0 +1,10 @@
+class User
+  include MongoMapper::Document
+  # Include default devise modules. Others available are:
+  # :token_authenticatable, :encryptable, :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+
+
+
+end
