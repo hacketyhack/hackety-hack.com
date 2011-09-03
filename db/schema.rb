c65956fa6691a8907fe196b96ff96c26688a9446
Dominic Dagradi <dominic@bearded.com>
2011-09-03 15:31:59 -0400

    removed unused files and schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,14 +11,6 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110830001940) do
-
-  create_table "questions", :force => true do |t|
-    t.string   "title"
-    t.text     "description"
-    t.integer  "user_id"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
+ActiveRecord::Schema.define(:version => 0) do
 
 end
