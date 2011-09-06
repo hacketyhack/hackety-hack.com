f6f85ec7957637947c982ee86c8aa67d475a490c
Steve Klabnik <steve@steveklabnik.com>
2011-09-06 19:05:54 -0400

    Basically all the specs have been autogenerated at this point.

--- spec/fabricators/program_fabricator.rb
+++
@@ -1,6 +0,0 @@
-Fabricator(:program) do
-  author_username "MyString"
-  slug "MyString"
-  title "MyString"
-  source_code "MyString"
-end
--- spec/fabricators/user_fabricator.rb
+++
@@ -1,5 +0,0 @@
-Fabricator(:user) do
-  email { sequence(:email) { |i| "user#{i}@example.com"} }
-  password "password"
-  password_confirmation "password"
-end
