8cddcf388653af5194f777954be1b75ba8dbbc21
Abhi Rao <abhirao@gmail.com>
2011-10-14 18:58:54 -0500

    Fixed routing issue and refactored following policy
    Refactored so that its reusable - just keep asking if it can follow another user rather than constructing a policy for each check

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,14 +3,15 @@ class UsersController < InheritedController
   skip_authorize_resource :only => [:following, :followers] #anyone can perform these read-only actions
 
   def follow
+    logger.info "reached follow"
     followee = User.first(:id => params[:user][:followee])
-    policy = FollowingPolicy.new(current_user, followee)
-    if policy.can_follow?
+    policy = FollowingPolicy.new(current_user)
+    if policy.can_follow? followee
       current_user.follow! followee
       notice = "You're following #{followee.username} now"
-    elsif policy.following_self?
+    elsif policy.following_self? followee
       notice = "You can't follow yourself silly!"
-    elsif policy.already_following?
+    elsif policy.already_following? followee
       notice = "You're already following #{followee.username}"
     end
     redirect_to resource_path(followee), :notice=> notice
@@ -21,5 +22,14 @@ class UsersController < InheritedController
     current_user.unfollow! followee
     redirect_to resource_path(followee), :notice=> "You're no longer following #{followee.username}"
   end
+  
+  def following
+    @user = User.first(:id => params[:user_id])  
+  end
+  
+  def followers
+    @user = User.first(:id => params[:user_id])
+  end
+  
   
 end
--- spec/following_policy_spec.rb
+++ spec/following_policy_spec.rb
@@ -2,25 +2,27 @@ require_relative '../app/models/following_policy'
 
 describe FollowingPolicy do
   before(:each) do 
-    @followed = stub()
+    @followee = stub()
     @follower = stub()
   end
   
   it 'can follow another' do
-    @follower.should_receive(:following?).with(@followed).and_return(false)
-    policy = FollowingPolicy.new(@follower, @followed)
-    policy.can_follow?.should be_true
+    @follower.should_receive(:following?).with(@followee).and_return(false)
+    policy = FollowingPolicy.new(@follower)
+    policy.can_follow?(@followee).should be_true
   end
   
   it "cannot follow self" do
-    policy = FollowingPolicy.new(@follower, @follower)
-    policy.can_follow?.should be_false
+    policy = FollowingPolicy.new(@follower)
+    policy.following_self?(@follower).should be_true
+    policy.can_follow?(@follower).should be_false
   end
   
   it "cannot follow twice" do
-    @follower.should_receive(:following?).with(@followed).and_return(true)
-    policy = FollowingPolicy.new(@follower,@followed)
-    policy.can_follow?.should be_false
+    @follower.should_receive(:following?).twice.with(@followee).and_return(true)
+    policy = FollowingPolicy.new(@follower)
+    policy.already_following?(@followee).should be_true
+    policy.can_follow?(@followee).should be_false
   end
   
 end
