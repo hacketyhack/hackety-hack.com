6b0c8c49c1a910a48a2205fefa0455140e086182
Steve Klabnik <steve@steveklabnik.com>
2011-10-21 18:32:34 -0400

    adding sendgrid

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -49,3 +49,14 @@ HacketyHackCom::Application.configure do
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
 end
+
+ActionMailer::Base.smtp_settings = {
+  :address        => 'smtp.sendgrid.net',
+  :port           => '587',
+  :authentication => :plain,
+  :user_name      => ENV['SENDGRID_USERNAME'],
+  :password       => ENV['SENDGRID_PASSWORD'],
+  :domain         => 'hackety-hack.com'
+}
+ActionMailer::Base.delivery_method = :smtp
+
