e3cd124045ce97443350bce99357b3f6b0634eb4
Steven! Ragnarök <steven@nuclearsandwich.com>
2011-10-24 22:14:29 -0700

    Add user link helpers that don't need an instantiated user.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def username_program_slug_path username, program_slug
+    "/users/#{username}/programs/#{program_slug}"
+  end
+
+  def username_path username
+    "/users/#{username}"
+  end
 end
