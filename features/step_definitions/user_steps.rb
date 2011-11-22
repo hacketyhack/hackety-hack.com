f92752f9d606df67f6b3228698859d44c82005d5
Rhys Powell <rpowell93@gmail.com>
2011-11-22 15:09:40 +1100

    Updated step definitions to use new #step method in place of Given/When/Then/And

--- features/step_definitions/user_steps.rb
+++ features/step_definitions/user_steps.rb
@@ -11,19 +11,19 @@ Given /^I am a user with username "([^"]*)" and password "([^"]*)"$/ do |usernam
 end
 
 Then /^I should be already signed in$/ do
-  And %{I should see "Logout"}
+  step %{I should see "Logout"}
 end
 
 Given /^I am signed up as "(.*)\/(.*)"$/ do |username, password|
-  Given %{I am not logged in}
-  When %{I go to the sign up page}
-  And %{I fill in "Username" with "#{username}"}
-  And %{I fill in "Email" with "#{username}@example.com"}
-  And %{I fill in "Password" with "#{password}"}
-  And %{I fill in "Password confirmation" with "#{password}"}
-  And %{I press "Sign up"}
-  Then %{I should see "You have signed up successfully. If enabled, a confirmation was sent to your e-mail."}
-  And %{I am logout}
+  step %{I am not logged in}
+  step %{I go to the sign up page}
+  step %{I fill in "Username" with "#{username}"}
+  step %{I fill in "Email" with "#{username}@example.com"}
+  step %{I fill in "Password" with "#{password}"}
+  step %{I fill in "Password confirmation" with "#{password}"}
+  step %{I press "Sign up"}
+  step %{I should see "You have signed up successfully. If enabled, a confirmation was sent to your e-mail."}
+  step %{I am logout}
 end
 
 Then /^I sign out$/ do
@@ -31,31 +31,31 @@ Then /^I sign out$/ do
 end
 
 Given /^I am logout$/ do
-  Given %{I sign out}
+  step %{I sign out}
 end
 
 Given /^I am not logged in$/ do
-  Given %{I sign out}
+  step %{I sign out}
 end
 
 When /^I sign in as "(.*)\/(.*)"$/ do |username, password|
-  Given %{I am not logged in}
-  When %{I go to the sign in page}
-  And %{I fill in "Username" with "#{username}"}
-  And %{I fill in "Password" with "#{password}"}
-  And %{I press "Sign in"}
+  step %{I am not logged in}
+  step %{I go to the sign in page}
+  step %{I fill in "Username" with "#{username}"}
+  step %{I fill in "Password" with "#{password}"}
+  step %{I press "Sign in"}
 end
 
 Then /^I should be signed in$/ do
-  Then %{I should see "Signed in successfully."}
+  step %{I should see "Signed in successfully."}
 end
 
 When /^I return next time$/ do
-  And %{I go to the home page}
+  step %{I go to the home page}
 end
 
 Then /^I should be signed out$/ do
-  And %{I should see "Sign up"}
-  And %{I should see "Login"}
-  And %{I should not see "Logout"}
+  step %{I should see "Sign up"}
+  step %{I should see "Login"}
+  step %{I should not see "Logout"}
 end
