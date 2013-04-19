45606ad84acd53fc55de129ddc18aa6ff9b35b76
Steven! Ragnarök <steven@nuclearsandwich.com>
2013-04-18 18:28:15 -0700

    Trade logic in Ruby for logic in YAML.
    I don't like this, I think it might be worth :skull:ing Mongo's
    `db:test:prepare` task and replacing it with a no op one. The logic in
    the config/mongo.yml file mirrors the logic in the Ruby initializer, but
    no one should rightfully expect logic to exist in the yml file.

--- config/initializers/mongo_mapper.rb
+++
@@ -1,9 +0,0 @@
-if ENV['MONGOHQ_URL']
-  MongoMapper.config = {
-    Rails.env => { 'uri' => ENV['MONGOHQ_URL'] }
-  }
-
-  MongoMapper.connect(Rails.env)
-else
-  MongoMapper.database = "hackety-hack-com-#{Rails.env}"
-end
