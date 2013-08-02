9d1f1c4e6c92b390045a9abbeb2d2f17ed9edd25
Angela Ebirim <cebirim@gmail.com>
2013-08-02 16:00:34 +0200

    new env.rb file

--- features/support/env.rb
+++ features/support/env.rb
@@ -3,7 +3,7 @@
 # newer version of cucumber-rails. Consider adding your own code to a new file 
 # instead of editing this one. Cucumber will automatically load all features/**/*.rb
 # files.
-
+#
 
 require 'simplecov'
 SimpleCov.start do
