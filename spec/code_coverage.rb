9aedb9634e2380dbfcae094829e5d63174fe3c5e
Tobias Pfeiffer <tobias.pfeiffer@student.hpi.uni-potsdam.de>
2013-08-05 19:47:16 +0200

    Added rspec filters back in as suggested by @trekr5 in #160

--- spec/code_coverage.rb
+++ spec/code_coverage.rb
@@ -8,4 +8,6 @@ SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter[
 SimpleCov.start do
   add_filter '/config/'
   add_filter '/vendor/'
+  add_filter '/spec/'
+  add_filter '/features/'
 end
