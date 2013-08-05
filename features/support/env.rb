91dea5bc7c4ce1b9ffce4fc50f0b52ad05c7790c
Angela Ebirim <cebirim@gmail.com>
2013-08-05 12:33:15 +0200

    env file with simplecov gem

--- features/support/env.rb
+++ features/support/env.rb
@@ -22,7 +22,7 @@ require 'cucumber/rails'
 # Capybara defaults to XPath selectors rather than Webrat's default of CSS3. In
 # order to ease the transition to Capybara we set the default here. If you'd
 # prefer to use XPath just remove this line and adjust any selectors in your
-# steps to use the XPath syntax.
+# steps to use the XPath syntax
 Capybara.default_selector = :css
 
 # By default, any exception happening in your Rails application will bubble up
