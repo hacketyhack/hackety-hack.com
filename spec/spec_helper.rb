ba814acc42c7bb53bf98eef5c19a2f9f44c4aa77
Angela Ebirim <cebirim@gmail.com>
2013-08-05 12:17:22 +0200

    spec helper file with simplecov gem

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -15,7 +15,7 @@ require 'database_cleaner'
 require 'capybara/rspec'
 
 # Requires supporting ruby files with custom matchers and macros, etc,
-# in spec/support/ and its subdirectories.
+# in spec/support/ and its subdirectories
 Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f}
 
 RSpec.configure do |config|
