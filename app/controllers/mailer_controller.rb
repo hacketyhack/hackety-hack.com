7d31ff7c9677da500dfb9bb709560d956f054b3a
Hari Carreras <haricarreras@gmail.com>
2013-08-28 17:52:32 +0200

    Diffusion works fine

--- app/controllers/mailer_controller.rb
+++ app/controllers/mailer_controller.rb
@@ -17,11 +17,13 @@ class MailerController < ApplicationController
   	@message = Message.new(params[:message])
 
   	if @message.valid?
-  		MessageMailer.new_message(@message).deliver
+      @message.email.each do |email|
+  		  MessageMailer.new_message(@message, email).deliver
+      end
   		redirect_to users_index_path, :notice => "Email sent correctly"
   	else
   		notice = "There was an error"
-  		reder :new
+  		render :new
   	end
   end
 end
--- app/mailers/message_mailer.rb
+++ app/mailers/message_mailer.rb
@@ -1,10 +1,9 @@
 class MessageMailer < ActionMailer::Base
   default from: "steve@hackety.com"
 
-  def new_message message 
+  def new_message message, email
   	@message = message
-  	@message.email.each do |email|
-  		mail(:to => email, :subject => @message.subject)
-  	end
+  	@email = email
+  	mail(:to => @email, :subject => @message.subject)
   end
 end
