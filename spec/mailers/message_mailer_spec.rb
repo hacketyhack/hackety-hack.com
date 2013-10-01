91cc7812ef2c2df14c287de3c269a8c2ecc3ce29
Hari Carreras <haricarreras@gmail.com>
2013-10-01 11:34:19 +0300

    Added some mail specs

--- spec/mailers/message_mailer_spec.rb
+++ spec/mailers/message_mailer_spec.rb
@@ -1,9 +1,21 @@
 require "spec_helper"
 
 describe MessageMailer do
-	let(:message) { MessageMailer.new_message(Fabricate(:message), Array("proof@example.com"))}
+  let(:message) { MessageMailer.new_message(Fabricate(:message), Array("proof@example.com"))}
 
   it "is from Steve" do
     message.from.should eq(["steve@hackety.com"])
+  end
+
+  it "Correct subject" do
+  	message.subject.should eq("Hackety-hack email")
+  end
+
+  it "Correct receiver email" do
+  	message.to.should eq(["proof@example.com"])
+  end
+
+  it "Correct body" do
+  	message.body.encoded.should match("This is an email from hackety-hack.com")
   end
 end
