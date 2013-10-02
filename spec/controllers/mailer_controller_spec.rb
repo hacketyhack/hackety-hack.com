6bcf49abda5fd674f2bfcdd9dab1fb873a5a62bc
Hari Carreras <haricarreras@gmail.com>
2013-10-02 17:54:09 +0300

    Added more specs

--- spec/controllers/mailer_controller_spec.rb
+++ spec/controllers/mailer_controller_spec.rb
@@ -2,6 +2,7 @@ require 'spec_helper'
 
 describe MailerController do
   let(:user) { Fabricate(:user) }
+  let(:users) { Fabricate.sequence(:user, 5)}
 
   describe "GET 'new' for a single user email" do
     it "returns http success" do
@@ -10,4 +11,24 @@ describe MailerController do
     end
   end
 
+  describe "GET 'new' for a diffusion" do
+    it "returns http success" do
+      get 'new', user: Array(users)
+      response.should be_success
+    end
+  end
+
+  describe "POST 'create' for a single user email" do
+    it "returns http success" do
+      get 'create', message: Fabricate.build(:message)
+      response.should be_success
+    end
+  end
+
+  describe "POST 'create' for a diffusion" do
+    it "returns http success" do
+      post 'create', message: Fabricate.build(:diffusion)
+      response.should be_success
+    end
+  end
 end
--- spec/fabricators/message_fabricator.rb
+++ spec/fabricators/message_fabricator.rb
@@ -3,3 +3,9 @@ Fabricator(:message) do
 	subject:"Hackety-hack email"
 	body:"This is an email from hackety-hack.com"
 end
+
+Fabricator(:diffusion, from: :message) do
+	email(count: 5) { |i| "proof#{i}@example.com" }
+	subject:"Hackety-hack email"	
+	body:"This is an email from hackety-hack.com"
+end
