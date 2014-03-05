32ffa202f773de5a292e40986c1e29cffe2e0561
Artur-Gaifutdinov <invictusru@gmail.com>
2014-03-04 20:24:45 -0500

    Rels and static controllers spec

--- spec/controllers/static_controller_spec.rb
+++ spec/controllers/static_controller_spec.rb
@@ -6,4 +6,19 @@ describe StaticController do
       expect(:get => "/").to route_to(:controller => "static", :action => "root")
     end
   end
+
+  describe "GET api_root" do
+    it "should render api layout" do
+      get :api_root
+      response.should render_template(:layout => "api")
+    end
+  end
+
+  describe "GET newest_version" do
+    it "should render version of api" do
+      get :newest_version
+      response.header['Content-Type'].should include 'application/json'
+      response.body.should include "1.0.0".to_json
+    end
+  end
 end
